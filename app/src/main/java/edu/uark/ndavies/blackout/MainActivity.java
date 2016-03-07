de89bc28ff6c8db80c2a90dbf3ca78d69fc64c7c
ndavies1173 <ndavies@uark.edu>
2016-03-07 09:15:59 -0600

    Added "Login" and "Sign Up" buttons to main activity

--- app/src/main/java/edu/uark/ndavies/blackout/MainActivity.java
+++ app/src/main/java/edu/uark/ndavies/blackout/MainActivity.java
@@ -2,6 +2,9 @@ package edu.uark.ndavies.blackout;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button login_button = (Button) findViewById(R.id.login_button);
+        login_button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+            }
+        });
+
+        final Button sign_up_button = (Button) findViewById(R.id.sign_up_button);
+        sign_up_button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+            }
+        });
     }
 }
