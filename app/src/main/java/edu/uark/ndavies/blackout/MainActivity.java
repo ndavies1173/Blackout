db79b2311472cebb5baf7120a0351371e26fe3db
ndavies1173 <ndavies@uark.edu>
2016-03-17 00:22:21 -0500

    Added basic login functionality, should just need to copy for sign_up and make sure correct values are passed to php.

--- app/src/main/java/edu/uark/ndavies/blackout/MainActivity.java
+++ app/src/main/java/edu/uark/ndavies/blackout/MainActivity.java
@@ -1,21 +1,52 @@
 package edu.uark.ndavies.blackout;
 
+import android.app.DownloadManager;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
+import android.widget.Toast;
 import android.widget.ViewAnimator;
 import android.widget.ViewFlipper;
+import android.widget.EditText;
+import android.widget.Button;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText email, password;
+    private Button login, sign_up;
+    private RequestQueue requestQueue;
+    private static final String URL = "http://192.168.1.65:80/something/user_control.php";
+    private StringRequest request;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        email = (EditText) findViewById(R.id.Email);
+        password = (EditText) findViewById(R.id.pass);
+
+        requestQueue = Volley.newRequestQueue(this);
 
         final ViewFlipper viewFlipper = (ViewFlipper) findViewById(R.id.viewFlipper);
 
@@ -23,6 +54,39 @@ public class MainActivity extends AppCompatActivity {
         login_button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 // Perform action on click
+                request = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            if(jsonObject.names().get(0).equals("success")){
+                                Toast.makeText(getApplicationContext(), "Success " +jsonObject.getString("success"), Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(getApplicationContext(), Home.class));
+                            }
+                            else{
+                                Toast.makeText(getApplicationContext(), "Error" +jsonObject.getString("error"), Toast.LENGTH_SHORT).show();
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                }){@Override
+                    protected Map<String, String> getParams() throws AuthFailureError{
+                    HashMap<String, String> hashMap = new HashMap<>();
+                    hashMap.put("email", email.getText().toString());
+                    hashMap.put("password", password.getText().toString());
+
+                    return hashMap;
+
+                }};
+
+                requestQueue.add(request);
             }
         });
 
